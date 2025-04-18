07ff427549a27b61a90588e484572b4d1bc581ee
sunainakharangate47 <sunaina@goaelectronics.co.in>
2025-04-18 10:29:10 +0530

    deleted constant

--- constants/index.js
+++
@@ -1,6 +0,0 @@
-const API_KEY_OpenWeather = "1523bfb5936a83689a5ba44bab03cb4d";
-const API_KEY_Weather = "0b32b0addb4f4084a6d111736251704";
-const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
-const GEO_URL = "https://api.openweathermap.org/geo/1.0/direct";
-
-export { API_KEY_OpenWeather, WEATHER_URL, GEO_URL, API_KEY_Weather };
