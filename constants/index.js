092c94a802c4105bc0e6a408456013a9e30a5adf
sunainakharangate47 <sunaina@goaelectronics.co.in>
2025-04-18 10:40:30 +0530

    changes

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+const API_KEY_OpenWeather = process.env.API_KEY_OpenWeather;
+const API_KEY_Weather = process.env.API_KEY_Weather;
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+const GEO_URL = "https://api.openweathermap.org/geo/1.0/direct";
+
+export { API_KEY_OpenWeather, WEATHER_URL, GEO_URL, API_KEY_Weather };
